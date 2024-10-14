cb658be6edd2b55d4749fd61c4e5e1d78b901210
phamkimkhuong <phamkhuong436@gmail.com>
2024-10-14 18:20:09 +0700

    create some components in project

---
+++ src/navigation/MainNavigator.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const Stack = createNativeStackNavigator();
+import HomeScreen from "../screens/HomeScreen";
+function MainNavigator() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{ headerShown: false }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default MainNavigator;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+const HomeScreen = () => {
+    const navigation = useNavigation();
+    return (
+        <View>
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+
+});
+export default HomeScreen;
