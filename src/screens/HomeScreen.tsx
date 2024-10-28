21e8fd9c9bb1b6e86fc87d68555adea493252197
phamkimkhuong <phamkhuong436@gmail.com>
2024-10-28 17:54:29 +0700

    create Header for HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,19 +1,71 @@
-import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Pressable,
+  ScrollView,
+  FlatList,
+} from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { data_avatar } from "../utils/const";
 const HomeScreen = () => {
-    const navigation = useNavigation();
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text style={styles.text}>HomeScreen</Text>
+  const navigation = useNavigation();
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <View style={styles.header}>
+            <Image source={require("../assets/home/logo.png")} />
+            <Text style={styles.title}>Video Sharing App</Text>
+          </View>
+          <Pressable
+            style={({ pressed }: { pressed: boolean }) => [
+              styles.pressable,
+              pressed && styles.pressed,
+            ]}
+            android_ripple={{ color: "lightgray" }}
+          >
+            <Ionicons name="notifications-outline" size={24} color="black" />
+          </Pressable>
         </View>
-    );
+        <View style={styles.banner}>
+          <FlatList data={data_avatar} />
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 30,
-        paddingHorizontal: 20,
-        paddingVertical: 30,
-    }
+  container: {
+    flex: 1,
+    marginVertical: 45,
+    marginHorizontal: 15,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  pressable: {
+    // Default style
+    opacity: 1,
+  },
+  pressed: {
+    opacity: 0.5, // Change when pressed
+  },
+  title: {
+    marginLeft: 10,
+    fontSize: 18,
+    fontWeight: "600",
+  },
+  banner: {},
+  text: {
+    fontSize: 30,
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
 });
 export default HomeScreen;
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,2 +1,39 @@
-
 export const PRIMARY_COLOR = "#f44b87";
+
+export const data_avatar = [
+  {
+    id: "1",
+    img: require("../assets/home/Image.png"),
+    name: "You",
+  },
+  {
+    id: "2",
+    img: require("../assets/home/avatar2.png"),
+    name: "Adam",
+  },
+  {
+    id: "3",
+    img: require("../assets/home/avatar3.png"),
+    name: "William",
+  },
+  {
+    id: "4",
+    img: require("../assets/home/avatar4.png"),
+    name: "Peter",
+  },
+  {
+    id: "5",
+    img: require("../assets/home/avatar5.png"),
+    name: "Julia",
+  },
+  {
+    id: "6",
+    img: require("../assets/home/avatar5.png"),
+    name: "Julia",
+  },
+  {
+    id: "7",
+    img: require("../assets/home/avatar5.png"),
+    name: "Julia",
+  },
+];
