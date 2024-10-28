f3e7b7187abc016a33b6e68fb5b20e18bffb4666
phamkimkhuong <phamkhuong436@gmail.com>
2024-10-28 21:39:26 +0700

    add Top trending and Browse topic in HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -10,7 +10,52 @@ import {
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { data_avatar } from "../utils/const";
+import { data_avatar, data_video, data_topic } from "../utils/const";
+
+const Item = ({ obj }: { obj: any }) => (
+  <Pressable
+    style={({ pressed }: { pressed: boolean }) => [
+      styles.pressable,
+      pressed && styles.pressed,
+    ]}
+  >
+    <View style={styles.iitem}>
+      <View style={styles.item}>
+        <Image source={obj.img} />
+        <Image style={styles.live} source={obj.live} />
+      </View>
+      <View>
+        <Text style={{ marginTop: 20 }}>{obj.name}</Text>
+      </View>
+    </View>
+  </Pressable>
+);
+const Item2 = ({ obj }: { obj: any }) => (
+  <Pressable
+    style={({ pressed }: { pressed: boolean }) => [
+      styles.pressable,
+      pressed && styles.pressed,
+    ]}
+  >
+    <View style={styles.iitem2}>
+      <Image source={obj.img} />
+    </View>
+  </Pressable>
+);
+const Item3 = ({ obj }: { obj: any }) => (
+  <Pressable
+    style={({ pressed }: { pressed: boolean }) => [
+      styles.pressable,
+      pressed && styles.pressed,
+    ]}
+  >
+    <View style={styles.item_topic}>
+      <Image source={obj.img} />
+      <Text style={{ marginTop: 10 }}>{obj.name}</Text>
+    </View>
+  </Pressable>
+);
+
 const HomeScreen = () => {
   const navigation = useNavigation();
   return (
@@ -32,7 +77,46 @@ const HomeScreen = () => {
           </Pressable>
         </View>
         <View style={styles.banner}>
-          <FlatList data={data_avatar} />
+          <FlatList
+            data={data_avatar}
+            renderItem={({ item }) => <Item obj={item} />}
+            keyExtractor={(item) => item.id}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+          />
+        </View>
+        <View style={styles.topTrending}>
+          <Text style={styles.textTrend}>Top trending</Text>
+          <Pressable
+            style={({ pressed }: { pressed: boolean }) => [
+              styles.pressable,
+              pressed && styles.pressed,
+            ]}
+          >
+            <Text style={{ color: "red", marginRight: 40 }}>View more</Text>
+          </Pressable>
+        </View>
+        <View>
+          <FlatList
+            data={data_video}
+            renderItem={({ item }) => <Item2 obj={item} />}
+            keyExtractor={(item) => item.id}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.center}
+          />
+        </View>
+        <View style={{}}>
+          <Text style={styles.textTrend}>Browse topic</Text>
+          <View>
+            <FlatList
+              data={data_topic}
+              renderItem={({ item }) => <Item3 obj={item} />}
+              keyExtractor={(item) => item.id}
+              numColumns={4}
+              columnWrapperStyle={styles.topic_data}
+            />
+          </View>
         </View>
       </View>
     </ScrollView>
@@ -42,7 +126,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginVertical: 45,
-    marginHorizontal: 15,
+    marginHorizontal: 20,
   },
   header: {
     flexDirection: "row",
@@ -61,11 +145,58 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "600",
   },
-  banner: {},
+  banner: {
+    marginTop: 15,
+  },
+  item: {
+    width: 40,
+    height: 40,
+  },
+  iitem: {
+    alignItems: "center",
+    marginRight: 20,
+  },
+  iitem2: {
+    marginRight: 15,
+    marginTop: 10,
+  },
+  center: {
+    alignItems: "center",
+  },
+  live: {
+    position: "absolute",
+    bottom: -14,
+    right: -13,
+  },
+
   text: {
     fontSize: 30,
     paddingHorizontal: 20,
     paddingVertical: 30,
+  },
+  textTrend: {
+    fontSize: 23,
+    fontWeight: "600",
+    opacity: 0.6,
+  },
+  topTrending: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+  topic_data: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginVertical: 8,
+  },
+  item_topic: {
+    backgroundColor: "#F8F9FA",
+    marginRight: 10,
+    padding: 10,
+    width: 75,
+    height: 70,
+    alignItems: "center",
   },
 });
 export default HomeScreen;
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -9,31 +9,95 @@ export const data_avatar = [
   {
     id: "2",
     img: require("../assets/home/avatar2.png"),
+    live: require("../assets/home/avarta_smail.png"),
     name: "Adam",
   },
   {
     id: "3",
     img: require("../assets/home/avatar3.png"),
+    live: require("../assets/home/avarta_smail.png"),
     name: "William",
   },
   {
     id: "4",
     img: require("../assets/home/avatar4.png"),
+    live: require("../assets/home/avarta_smail.png"),
     name: "Peter",
   },
   {
     id: "5",
     img: require("../assets/home/avatar5.png"),
+    live: require("../assets/home/avarta_smail.png"),
     name: "Julia",
   },
   {
     id: "6",
     img: require("../assets/home/avatar5.png"),
+    live: require("../assets/home/avarta_smail.png"),
     name: "Julia",
   },
   {
     id: "7",
     img: require("../assets/home/avatar5.png"),
+    live: require("../assets/home/avarta_smail.png"),
     name: "Julia",
+  },
+];
+
+export const data_video = [
+  {
+    id: "1",
+    img: require("../assets/home/trending.png"),
+  },
+  {
+    id: "2",
+    img: require("../assets/home/trending2.png"),
+  },
+  {
+    id: "3",
+    img: require("../assets/home/trending3.png"),
+  },
+];
+
+export const data_topic = [
+  {
+    id: "1",
+    img: require("../assets/home/topic1.png"),
+    name: "Sports",
+  },
+  {
+    id: "2",
+    img: require("../assets/home/topic2.png"),
+    name: "Podcasts",
+  },
+  {
+    id: "3",
+    img: require("../assets/home/topic3.png"),
+    name: "News",
+  },
+  {
+    id: "4",
+    img: require("../assets/home/topic4.png"),
+    name: "Travel",
+  },
+  {
+    id: "5",
+    img: require("../assets/home/topic5.png"),
+    name: "Health",
+  },
+  {
+    id: "6",
+    img: require("../assets/home/topic6.png"),
+    name: "Weather",
+  },
+  {
+    id: "7",
+    img: require("../assets/home/topic7.png"),
+    name: "Art",
+  },
+  {
+    id: "8",
+    img: require("../assets/home/topic8.png"),
+    name: "Topics",
   },
 ];
